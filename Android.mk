716d98d979f83bfafdd56db2ce2bbb9810dc4283
ZVNexus <ZVNexus@users.noreply.github.com>
2022-01-27 18:02:17 +0530

    mi-common: Add missing EGL symlinks
    Change-Id: Icb45651c5b5254dbc2053faa48f64f824e9d9902
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- Android.mk
+++ Android.mk
@@ -133,13 +133,27 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_FAKE)
 LOCAL_REQUIRED_MODULES := $(MITHORIUM_PRODUCT_PACKAGES)
 include $(BUILD_PHONY_PACKAGE)
 
-EGL_SYMLINKS := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so \
-    $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so \
-    $(TARGET_OUT_VENDOR)/lib64/libEGL_adreno.so
-$(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+EGL_LIBRARIES := \
+	libEGL_adreno.so \
+	libGLESv2_adreno.so \
+	libq3dtools_adreno.so
+
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBRARIES)))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
 	@mkdir -p $(dir $@)
+	@rm -rf $@
 	$(hide) ln -sf egl/$(notdir $@) $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINKS)
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBRARIES)))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(EGL_32_SYMLINKS) \
+	$(EGL_64_SYMLINKS)
 
 endif
